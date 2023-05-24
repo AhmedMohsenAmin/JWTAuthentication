24b2764d88fdcb219a573a8103bb07874e67e4c6
DELL <DELL@DESKTOP-6EBS5JJ>
2023-05-24 22:08:16 +0300

    Add project files.

---
+++ JWTAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+            if (user != null)
+            {
+                var token = GenerateToken(user);
+                return Ok(token);
+            }
+
+            return NotFound("user not found");
+        }
+
+
+
+        // To generate token
+        private string GenerateToken(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.UserName),
+                new Claim(ClaimTypes.Role,user.Role)
+            };
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+        }
+
+        //To authenticate user
+        private UserModel Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.users.FirstOrDefault(x => x.UserName.ToLower() ==
+                userLogin.UserName.ToLower() && x.Password == userLogin.Password);
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+            return null;
+        }
+    }
+}    
---
+++ JWTAuthentication/Models/UserConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace JWTAuthentication.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> users = new()
+        {
+            new UserModel()
+            {
+                UserName = "Mohsen",
+                Password = "123456",
+                Role = "Admin"
+            },
+             new UserModel()
+            {
+                UserName = "Megz",
+                Password = "123456",
+                Role = "User"
+            }
+        };
+    }
+}
